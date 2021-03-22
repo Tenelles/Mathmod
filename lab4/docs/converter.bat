1485d39c7ee08c54125600f92c124d4a603050b4
Tenelles <tenelles@yandex.ru>
2021-03-22 11:43:18 +0300

    chore: fix lab4

--- lab4/docs/converter.bat
+++
@@ -1,3 +0,0 @@
-pandoc --filter pandoc-crossref -o report.docx report.md
-pandoc --filter pandoc-crossref -o report.pdf report.md
-pandoc --number-sections -t beamer --slide-level=2 --filter pandoc-crossref -o presentation.pdf presentation.md
